9adf64d65e5c81d4d423bb08238c823e4097e720
yangjie403 <yang420jie@gmail.com>
2026-08-29 01:32:04 +0800

    DrawerLayout+Motion动画示例

---
+++ app/src/main/java/com/mjieg/composables/activities/DrawerLayoutMotionActivity.kt
@@ -0,0 +1,13 @@
+package com.mjieg.composables.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.mjieg.composables.R
+
+class DrawerLayoutMotionActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_drawerlayout)
+    }
+}
---
+++ app/src/main/java/com/mjieg/composables/views/DrawerContent.kt
@@ -0,0 +1,32 @@
+package com.mjieg.composables.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.drawerlayout.widget.DrawerLayout
+
+class DrawerContent @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : MotionLayout(context, attrs, defStyleAttr), DrawerLayout.DrawerListener {
+    override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+        progress = slideOffset
+    }
+
+    override fun onDrawerOpened(drawerView: View) {
+    }
+
+    override fun onDrawerClosed(drawerView: View) {
+    }
+
+    override fun onDrawerStateChanged(newState: Int) {
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        (parent as? DrawerLayout)?.addDrawerListener(this)
+    }
+
+}
